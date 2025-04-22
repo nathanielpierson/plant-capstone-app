b0e03ecf515386f274476246ba31440d8e139948
nathanielpierson <nathanielepierson@gmail.com>
2025-04-22 08:20:18 -0400

    fix error with plant_count code in wrong place

--- app/controllers/plant_counts_controller.rb
+++ app/controllers/plant_counts_controller.rb
@@ -1,2 +1,12 @@
 class PlantCountsController < ApplicationController
+  def index
+    Plant_count.all
+    render :index
+  end
+  def update
+    @plant_count = Plant_count.find_by(id: params[:id])
+    @plant_count.update (
+
+    )
+  end
 end
--- app/models/plant_count.rb
+++ app/models/plant_count.rb
@@ -1,12 +1,2 @@
 class PlantCount < ApplicationRecord
-  def index
-    Plant_count.all
-    render :index
-  end
-  def update
-    @plant_count = Plant_count.find_by(id: params[:id])
-    @plant_count.update (
-
-    )
-  end
 end
