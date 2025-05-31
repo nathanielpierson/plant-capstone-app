3327159dd745d79242518da8cabeb5aa2501b5e8
nathanielpierson <nathanielepierson@gmail.com>
2025-05-30 23:14:16 -0400

    adjustments for Render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   allow_browser versions: :modern
+  skip_before_action :verify_authenticity_token
   def current_user
     auth_headers = request.headers["Authorization"]
     if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
